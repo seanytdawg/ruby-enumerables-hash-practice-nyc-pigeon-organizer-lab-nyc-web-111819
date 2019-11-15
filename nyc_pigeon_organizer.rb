3631cd8896d002e30b153755130d129078f26e5e
Sean Tarzy <seantarzy@gmail.com>
2019-11-15 19:15:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
+require "pry" 
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |k, v|
+    v.each do |k2, v2|
+      v2.each do |name|
+       new_hash[name] =  {k => [k2]}
+    end 
+  end 
+  end 
+   binding.pry
+  return new_hash
 end
