304c73a0eeb1e86e2484f5a26fe67ee1f4738742
Sean Tarzy <seantarzy@gmail.com>
2019-11-15 19:22:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,12 +2,16 @@ require "pry"
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |k, v|
-    v.each do |k2, v2|
-      v2.each do |name|
-       new_hash[name] =  {k => [k2]}
+    v.each do |k2, names|
+      names.each do |name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end 
+        binding.pry 
+        # if new_hash[]
+      # new_hash[name] =  {k => [k2]}
     end 
   end 
   end 
-   binding.pry
   return new_hash
 end
