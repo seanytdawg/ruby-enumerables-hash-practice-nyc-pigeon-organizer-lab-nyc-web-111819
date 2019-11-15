2e60a36902c316b6abc4c4879e4b81af175d17d2
Sean Tarzy <seantarzy@gmail.com>
2019-11-15 19:47:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require "pry" 
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |k, v|
+    v.each do |k2, names|
+      names.each do |name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end 
+        if !new_hash[name][k]
+          new_hash[name][k] = []
+        end 
+      new_hash[name][k] << k2.to_s  
+    end 
+  end 
+  end 
+  return new_hash
 end
